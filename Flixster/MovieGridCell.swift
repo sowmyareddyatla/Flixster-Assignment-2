a55cb940cb7238cf0a99ad9c4da31e67510d07ba
sowmya reddy <atlasowmya14@gmail.com>
2021-07-26 01:22:17 -0500

    Initial Commit

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Sowmya Reddy Atla on 7/22/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
